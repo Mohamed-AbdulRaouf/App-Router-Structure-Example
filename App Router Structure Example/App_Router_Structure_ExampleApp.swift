1dfe1f8382aa8e6b19dea61693b8bc506744c47f
Raouf <mabdulraouf@ipmagix.com>
2025-05-25 11:25:12 +0300

    Initial Commit

---
+++ App Router Structure Example/App_Router_Structure_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Router_Structure_ExampleApp.swift
+//  App Router Structure Example
+//
+//  Created by Raouf on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct App_Router_Structure_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
